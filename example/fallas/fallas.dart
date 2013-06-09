ae632b137c794dfc81a81c6eb7c31db34e35cd7a
jcmellado <jcmellado@gmail.com>
2013-06-09 10:41:37 +0000

    Initial import

---
+++ example/fallas/fallas.dart
@@ -0,0 +1,34 @@
+/*
+  Copyright (c) 2013 Juan Mellado
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+import "dart:html";
+import "package:neuquant/neuquant.dart";
+
+void main() {
+  var image = query("#image") as ImageElement;
+  var canvas = query("#canvas") as CanvasElement;
+  var context = canvas.context2D;
+  context.drawImage(image, 0, 0);
+  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  neuquant(imageData.data, 10);
+  context.putImageData(imageData, 0, 0);
+}
